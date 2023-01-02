806b781db32dc24aa7a60d860e8c644af5b8689a
Nikolai Anufriev <nikolanius13@gmail.com>
2023-01-03 01:54:32 +0300

    Написали программу которая выводит массив с элементами заданными пользователем через консоль

--- Final Ex/Program.cs
+++ Final Ex/Program.cs
@@ -1,3 +1,23 @@
 ﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует массив строк, длинна которых меньше либо равна 3 символа. 
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.  */
 
+int n;
+Console.Write($"Введите количество элементов массива:  ");
+int.TryParse(Console.ReadLine()!, out n);
+
+string[] array = new string[n];
+string h;
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент № {i + 1} ");
+    h = Console.ReadLine()!;
+    array[i] = h;
+}
+
+Console.WriteLine($"Заданный массив ->  ");
+
+for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+
