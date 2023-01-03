a25c0ff13dc76798b192d716d9d7a0ed15a583fc
Nikolai Anufriev <nikolanius13@gmail.com>
2023-01-03 23:39:59 +0300

    Немного подправил код программы

--- Final Ex/Program.cs
+++ Final Ex/Program.cs
@@ -2,22 +2,20 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.  */
 
 int n;
-Console.Write($"Введите количество элементов массива:  ");
+Console.Write($"Введите общее количество элементов (где 1 элемент = одной цифре, числу, слову или набору разнообразных символов) массива:  ");
 int.TryParse(Console.ReadLine()!, out n);
 
 string[] array = new string[n];
-string h;
-
 
 for (int i = 0; i < array.Length; i++)
 {
     Console.Write($"Введите элемент № {i + 1} ");
-    h = Console.ReadLine()!;
-    array[i] = h;
+    array[i] = Console.ReadLine()!;
 }
-
+Console.WriteLine();
 Console.WriteLine($"Заданный массив ->  ");
 
 for (int i = 0; i < array.Length; i++)
     Console.Write($"{array[i]} ");
+Console.WriteLine("\n");
 
