1ebab9767717d2af911b14fb668d01996012ec72
Nikolai Anufriev <nikolanius13@gmail.com>
2023-01-04 21:46:10 +0300

    Исправил код программы, вывел 2 способа решения задачи и оформил текст для лутшего восприятия

--- Final Ex/Program.cs
+++ Final Ex/Program.cs
@@ -1,7 +1,11 @@
 ﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует массив строк, длинна которых меньше либо равна 3 символа. 
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.  */
 
-int n;
+// Можно выполнить 2 - мя способами, но более правильным считаю второй.
+
+//                                           Способ № 1
+
+/* int n;
 Console.Write($"Введите общее количество элементов (где 1 элемент = одной цифре, числу, слову или набору разнообразных символов) массива:  ");
 int.TryParse(Console.ReadLine()!, out n);
 
@@ -17,5 +21,22 @@ Console.WriteLine($"Заданный массив ->  ");
 
 for (int i = 0; i < array.Length; i++)
     Console.Write($"{array[i]} ");
-Console.WriteLine("\n");
+Console.WriteLine("\n"); 
 
+Console.WriteLine($"Отсортированный массив ->  ");
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3)
+    Console.Write($"{array[i]} ");
+Console.WriteLine("\n"); */
+
+//                                            Способ № 2 
+
+Console.WriteLine($"Введите элементы через пробел, для завершения ввода нажмите 'Enter' ");
+string[] array = Console.ReadLine()!.Split(' ');
+
+Console.WriteLine($"Отсортированный массив ->  ");
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3)
+    Console.Write($"{array[i]} ");
+Console.WriteLine("\n");
